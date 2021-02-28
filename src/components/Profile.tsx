d77f9acebbd973b0b3b62f92c1433906de8c9e78
Fabio Bandacheski <fabio.versao.beta@gmail.com>
2021-02-28 12:07:29 -0300

    my profile data

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -7,9 +7,9 @@ export function Profile() {
 
     return (
         <div className={styles.profileContainer}>
-            <img src="https://github.com/diego3g.png" alt="Diego Fernandes" />
+            <img src="https://avatars.githubusercontent.com/u/18427563?s=460&u=1c4c883ea72d5d7dc9bb49dc9992942f3eb28aa6&v=4" alt="Fabio Bandacheski" />
             <div>
-                <strong>Diego Fernandes</strong>
+                <strong>Fabio Bandacheski</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
                     Level {level}
